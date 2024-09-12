df33b412857cd2d3fc2d149c43974f963195bb65
arinnixx <tyrtyshnaya.a.0@mail.ru>
2024-09-12 21:23:20 +0300

    Initial commit

---
+++ src/Main.kt
@@ -0,0 +1,9 @@
+import kotlin.math.pow
+import kotlin.math.log
+fun main() {
+    println("Введите два числа: ")
+    var n= readln().toDouble()
+    var x= readln().toDouble()
+    var y=log(x,n)
+    print(y)
+}
